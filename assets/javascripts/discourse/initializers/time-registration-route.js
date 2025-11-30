807268c3cbcc78a72ad411716b6ee59caac4ebbc
Richard <richard@communiteq.com>
2025-11-30 22:33:36 +0000

    Fix: remove runaway registerConnectorClass file

--- assets/javascripts/discourse/initializers/time-registration-route.js
+++
@@ -1,9 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("0.8", (api) => {
-  api.registerConnectorClass("user-profile-primary", "time-registration-report", {
-    shouldRender(args, component) {
-      return component.currentUser && component.currentUser.admin; // Or check group
-    }
-  });
-});
